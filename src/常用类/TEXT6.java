82d5ddfad19a6ed55c71f49b4a36de117fe1f7ab
huangdingluan <316943766@qq.com>
2019-05-01 15:56:36 +0800

    2

--- src/常用类/TEXT6.java
+++ src/常用类/TEXT6.java
@@ -6,14 +6,20 @@ public class TEXT6 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 int[] num = new int[10];
+		 int[] arr = new int[10];
 	        Random r = new Random();
 	        System.out.println("产生的随机数 : ");
 	        for (int i = 0; i < 10; i++) {
-	            num[i] = r.nextInt(100);
-	            System.out.print(num[i] + " ");
+	        	arr[i] = r.nextInt(100);
+	            System.out.print(arr[i] + " ");
 	        }
 	        System.out.println();
+	        
+	        int arr1[]=new int[10];
+			System.arraycopy(arr, 0, arr1, 0, 10);//将arr复制到arr1中
+			for(int i=0;i<10;i++) {			
+				System.out.println(arr1[i]);
+			}
 
 	}
 
